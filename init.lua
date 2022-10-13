b16daa1ee354146c996e39c29cbb2be4bfcac098
Mario <marioanticoli@gmail.com>
2022-10-14 01:50:08 +0200

    add border to floating window

--- init.lua
+++ init.lua
@@ -6,6 +6,20 @@
 vim.g.mapleader = ";"
 vim.g.localleader = "\\"
 
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
+  vim.lsp.handlers.hover, {
+    -- Use a sharp border with `FloatBorder` highlights
+    border = "single"
+  }
+)
+
+vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
+  vim.lsp.handlers.signature_help, {
+    -- Use a sharp border with `FloatBorder` highlights
+    border = "single"
+  }
+)
+
 -- IMPORTS
 require('vars')      -- Variables
 require('opts')      -- Options
