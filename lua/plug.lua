e05cf544d5be0b8ff275aee33a3620aa50473dce
Mario Anticoli <mario.anticoli@fcm.digital>
2022-12-07 23:09:42 +0100

    fix small config errors

--- lua/plug.lua
+++ lua/plug.lua
@@ -49,7 +49,7 @@ return require('packer').startup(function(use)
     use 'hrsh7th/cmp-buffer'
     use 'hrsh7th/cmp-path'
     use 'hrsh7th/cmp-cmdline'
-    use({'L3MON4D3/LuaSnip', tag = 'v<CurrentMajor>.*'})
+    use 'L3MON4D3/LuaSnip'
     use {
         'weilbith/nvim-code-action-menu',
         cmd = 'CodeActionMenu' }
