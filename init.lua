76913f23960374f75d6a733bfddceb3b5454d81b
Mario Anticoli <mario.anticoli@fcm.digital>
2022-12-09 12:53:08 +0100

    add js parser by default

--- init.lua
+++ init.lua
@@ -58,6 +58,7 @@ require('impatient')
 
 require'nvim-treesitter.configs'.setup {
   ensure_installed = {
+    "javascript"
   },
   highlight = {
     enable = true,
--- language-template-config/elixir-nvimrc.lua
+++ language-template-config/elixir-nvimrc.lua
@@ -4,8 +4,9 @@ require'nvim-treesitter.configs'.setup {
     "elixir",
     "erlang",
     "heex",
-    "html"
-  },
+    "html",
+    "javascript"
+ },
   highlight = {
     enable = true,
   },
