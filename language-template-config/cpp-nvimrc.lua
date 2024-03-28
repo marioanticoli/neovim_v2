919b0dc20141614a34a488b40a14c33d5fefb317
Mario Anticoli <mario.anticoli@fcm.digital>
2024-03-28 20:56:52 +0100

    improve key bindings

--- language-template-config/cpp-nvimrc.lua
+++ language-template-config/cpp-nvimrc.lua
@@ -1,5 +1,28 @@
+-- Remember to link with name .nvimrc.lua
+
+require'nvim-treesitter.configs'.setup {
+  ensure_installed = {
+    "c",
+    "cpp",
+    "arduino"
+  },
+  highlight = {
+    enable = true,
+  },
+  indent = {
+    enable = true,
+  },
+  refactor = {
+    highlight_definitions = {
+      enable = true,
+      clear_on_cursor_move = true,
+    }
+  }
+}
+
 -- CPP LSP 
-require('lspconfig')['ccls'].setup {
+lspconfig.clangd.setup {
+  filetypes = { "c", "cpp", "objc", "objcpp", "cuda", "proto" },
   init_options = {
     compilationDatabaseDirectory = "build";
     index = {
