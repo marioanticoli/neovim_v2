446270c97a07e870646aea4c93973997dc139f6f
Mario Anticoli <marioanticoli@gmail.com`>
2025-07-28 09:47:04 +0200

    aider lazy

--- init.lua
+++ init.lua
@@ -246,12 +246,37 @@ require("lazy").setup({
     lazy = true
   },
   {
-    "Exafunction/windsurf.vim",
-    --dependencies = {
-      --"nvim-lua/plenary.nvim",
-      --"hrsh7th/nvim-cmp",
-    --},
-    lazy = true
+    "GeorgesAlkhouri/nvim-aider",
+    lazy = true,
+    cmd = "Aider",
+    -- Example key mappings for common actions:
+    keys = {
+      { "<leader>a/", "<cmd>Aider toggle<cr>", desc = "Toggle Aider" },
+      { "<leader>as", "<cmd>Aider send<cr>", desc = "Send to Aider", mode = { "n", "v" } },
+      { "<leader>ac", "<cmd>Aider command<cr>", desc = "Aider Commands" },
+      { "<leader>ab", "<cmd>Aider buffer<cr>", desc = "Send Buffer" },
+      { "<leader>a+", "<cmd>Aider add<cr>", desc = "Add File" },
+      { "<leader>a-", "<cmd>Aider drop<cr>", desc = "Drop File" },
+      { "<leader>ar", "<cmd>Aider add readonly<cr>", desc = "Add Read-Only" },
+      { "<leader>aR", "<cmd>Aider reset<cr>", desc = "Reset Session" },
+      -- Example nvim-tree.lua integration if needed
+      { "<leader>a+", "<cmd>AiderTreeAddFile<cr>", desc = "Add File from Tree to Aider", ft = "NvimTree" },
+      { "<leader>a-", "<cmd>AiderTreeDropFile<cr>", desc = "Drop File from Tree from Aider", ft = "NvimTree" },
+    },
+    dependencies = {
+      "folke/snacks.nvim",
+      --- The below dependencies are optional
+      "catppuccin/nvim",
+      "nvim-tree/nvim-tree.lua",
+      --- Neo-tree integration
+      --{
+        --"nvim-neo-tree/neo-tree.nvim",
+        --opts = function(_, opts)
+          --require("nvim_aider.neo_tree").setup(opts)
+        --end,
+      --},
+    },
+    config = true,
   },
   {
     "zbirenbaum/copilot.lua",
@@ -345,8 +370,7 @@ cmp.setup({
     { name = "path" },
     { name = 'nvim_lsp_signature_help' },
     { name = "luasnip" },
-    { name = "copilot", group_index = 2 },
-    { name = "codeium" }
+    { name = "copilot", group_index = 2 }
   })
 })
 
